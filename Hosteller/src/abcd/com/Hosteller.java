7b86ed3c2884ec27c060a0dc411ec66bd6bcec9c
001ZPZ744 <001ZPZ744@LAPTOP-66UL3OFP>
2021-06-10 21:16:00 +0530

    Project Hostel

---
+++ Hosteller/src/abcd/com/Hosteller.java
@@ -0,0 +1,30 @@
+package abcd.com;
+
+public class Hosteller {
+	
+	 private String hostelName;
+	    private int roomNumber;
+	    public Hosteller()
+	    {
+	        hostelName=" ";
+	        roomNumber=0;
+	    }
+	    public void setHostelName(String x)
+	    {
+	        hostelName=x;
+	    }
+	    public void setRoomNumber(int y)
+	    {
+	        roomNumber=y;
+	    }
+	    public String getHostelName()
+	    {
+	        return hostelName;
+	    }
+	    public int getRoomNumber()
+	    {
+	        return roomNumber;
+	    }
+		
+
+}
