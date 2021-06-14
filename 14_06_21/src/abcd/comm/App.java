afe6d94b1187e3f89180491e72c778e7a93b603b
001ZPZ744 <001ZPZ744@LAPTOP-66UL3OFP>
2021-06-14 11:26:31 +0530

    vector implemnted

---
+++ 14_06_21/src/abcd/comm/App.java
@@ -0,0 +1,39 @@
+package abcd.comm;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+import java.util.TreeSet;
+import java.util.UUID;
+import java.util.Vector;
+
+public class App {
+public static void main(String[] args) {
+		
+//		
+//		  Vector<Student> vector=new Vector<Student>(); vector.add(new Student(1,
+//		  "John", 5.5)); vector.add(new Student(2, "Marry", 5.7)); vector.add(new
+//		  Student(3, "Public", 4.9)); System.out.println(vector); //enhanched for loop
+//		  for(Student s:vector) { System.out.println(s); } //enum implementation
+//		  Enumeration<Student> e=vector.elements(); while(e.hasMoreElements()) {
+//		  System.out.println(e.nextElement()); } //iterator implementation
+//		  Iterator<Student> i=vector.iterator(); while(i.hasNext()) {
+//		  System.out.println(i.next()); } Stack<Student> stack=new Stack<Student>();
+//		  stack.push(new Student(1, "John", 5.5)); stack.push(new Student(2, "Marry",
+//		  5.7)); int v=stack.capacity(); System.out.println(v);
+//		  System.out.println(stack); System.out.println(stack.pop());
+//		  System.out.println(stack); System.out.println(stack.peek());
+//		  System.out.println(stack); Collection<Student> colllCollection=new
+//		  ArrayList<>(); colllCollection.add(new Student()); colllCollection.add(new
+//		  Student()); stack.addAll(colllCollection); System.out.println("=========\n");
+//		  Enumeration<Student> s=stack.elements(); while(s.hasMoreElements()) {
+//		  System.out.println(s.nextElement()); }
+		  
+		  
+		 
+}
+
+}
