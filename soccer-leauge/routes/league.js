ef11503d0d8d89f83c8ae8a360cfefe8a2f82d28
Tendulkar Pal <77629135+tendulkar1997@users.noreply.github.com>
2021-07-04 21:37:07 +0530

    Delete league.js

--- soccer-leauge/routes/league.js
+++
@@ -1,9 +0,0 @@
-const path=require('path');
-const express=require('express');
-const app=express();
-const router=express.Router();
-router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(__dirname,'../','views','soccer-league.html'));    
-
-});
-module.exports=router;
