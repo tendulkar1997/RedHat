2ba951658b2a0d739eb5a538ebde5a9e2e0f7381
Tendulkar Pal <77629135+tendulkar1997@users.noreply.github.com>
2021-07-06 21:55:53 +0530

    Add files via upload

---
+++ before vidlay/index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const genres = require('./routes/genre');
+const customers = require('./routes/customer');
+const express = require('express');
+const app = express();
+
+mongoose.connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
+app.use(express.json());
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
+app.listen(3000);
---
+++ before vidlay/routes/genre.js
@@ -0,0 +1,42 @@
+const {Genre, validate} = require('../models/genre');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const genres = await Genre.find().sort('name');
+  res.send(genres);
+});
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let genre = new Genre({ name: req.body.name });
+  genre = await genre.save();
+  
+  res.send(genre);
+});
+router.get('/:id', async (req, res) => {
+  const genre = await Genre.findById(req.params.id).catch((err)=>{
+    res.status(404).send('The genre with the given ID was not found.');
+
+  });
+
+  if (!genre) 
+    return res.status(404).send('The genre with the given ID was not found.');
+
+  res.send(genre);
+});
+
+router.delete('/:id', async (req, res) => {
+  const genre = await Customer.findByIdAndRemove(req.params.id).catch((err)=>{
+    res.status(404).send('The genre with the given ID was not found.');
+  });
+
+  if (!genre) 
+    return res.status(404).send('The genre with the given ID was not found.');
+
+  res.send(genre);
+});
+module.exports = router
