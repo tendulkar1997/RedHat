abc9b648175c0e42d8d736af3d707a2212b8da01
Tendulkar Pal <77629135+tendulkar1997@users.noreply.github.com>
2021-07-12 22:02:07 +0530

    Add files via upload

---
+++ item-service/src/main/java/com/example/demo/model/Item.java
@@ -0,0 +1,35 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "item_table")
+public class Item {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	@Column(name = "item_number", unique = true)
+	private String itemNumber;
+	@Column(name = "item_name")
+	private String itemName;
+	@Column(name = "item_price")
+	private Double itemPrice;
+	@JsonProperty
+	@Column(name = "available")
+	private Boolean isAvailable;
+
+}
