8ab4cb7b3407f7302ca37eed645edad6d9b4a1ef
Tendulkar Pal <77629135+tendulkar1997@users.noreply.github.com>
2021-07-29 14:40:29 +0530

    Add files via upload

---
+++ junit-assertions-demo/src/test/java/com/example/junit_assertions_demo/AppTest.java
@@ -0,0 +1,16 @@
+package com.example.junit_assertions_demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+       Assertions.assertTrue( true );
+    }
+}
